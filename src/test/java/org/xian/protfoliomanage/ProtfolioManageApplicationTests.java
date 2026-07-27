2d2acf3ce4287a5db40f1857cbb62286fda9f679
viggo <1813209344@qq.com>
2026-07-27 05:17:01 +0000

    Create Springboot project skeleton

---
+++ src/test/java/org/xian/protfoliomanage/ProtfolioManageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xian.protfoliomanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtfolioManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
